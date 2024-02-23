0942b6d84649cd2b63ed06b2dc84227c3b4a6a98
Salehi <apple@Apples-MacBook-Air.local>
2024-02-23 21:24:19 +0330

    upddate

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 import "./ProductCard.css";
 import { Product } from "./Products.type";
+import { CartContext } from "../Context/CartContext";
+import swal from "sweetalert";
+import { useNavigate } from "react-router-dom";
 
-export default function ProductCard({ title, price, image, rating }: Product) {
+export default function ProductCard({
+  title,
+  price,
+  image,
+  rating,
+  id,
+}: Product) {
   const { rate, count } = rating;
-
+  const context = useContext(CartContext);
+  const navigate = useNavigate();
   // محاسبه تعداد ستاره‌های کامل
   const fullStars = Math.floor(rate);
-
   // محاسبه تعداد ستاره‌های نیمه‌پر
   const halfStars = Math.round(rate - fullStars) % 2;
-
   // محاسبه تعداد ستاره‌های خالی
   const emptyStars = 5 - fullStars - halfStars;
-
+  const addProductHandler = () => {
+    context.addProduct(id);
+    swal({
+      title: "Product has been added to the cart",
+      icon: "success",
+      buttons: ["Ok", "Go to cart"],
+    }).then((result) => {
+      if (result) {
+        navigate("/cart");
+      }
+    });
+  };
   return (
     <div className="card">
       <img src={image} alt={title} />
@@ -34,7 +53,7 @@ export default function ProductCard({ title, price, image, rating }: Product) {
           </div>
           <p>${price}</p>
         </div>
-        <button>Add to Basket</button>
+        <button onClick={addProductHandler}>Add to Basket</button>
       </main>
     </div>
   );
